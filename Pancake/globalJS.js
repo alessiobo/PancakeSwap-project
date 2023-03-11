589e35ba4d62ef7ebd654f96a3b895a1adab0847
Cipriano Mereu <ciprianomereu@gmail.com>
2023-03-11 17:59:10 +0100

    start: dark-mode cards-section

--- Pancake/globalJS.js
+++ Pancake/globalJS.js
@@ -12,6 +12,11 @@ inputChangeMode.addEventListener("change", () => {
   const herop = document.querySelector(".hero-homepage-text-parag");
   const heroWave = document.getElementById("hero-wave");
 
+  //CARD_SECTION_DARK-----------------------------------
+
+  const cardSection = document.querySelector(".card-section");
+  const cardTitleH2 = document.querySelector(".card-title-h2");
+
   //TWIN_SECTION_DARK-----------------------------------
   const twinbg = document.querySelector(".section3-wrapper");
   const twin3title = document.querySelector(".twin-section-title");
@@ -33,23 +38,25 @@ inputChangeMode.addEventListener("change", () => {
 
     //HERO_SECTION_DARK
 
-    const herobg = document.querySelector(".hero-section");
-    const herop = document.querySelector(".hero-homepage-text-parag");
-
     herobg.classList.add("hero-section-dark");
     herop.classList.add("hero-homepage-text-parag-dark");
     heroWave.src = "./Components/HeroSection/images/wave-bg-d.svg";
 
+    //CARDS_SECTION_DARK
+    cardSection.classList.add("card-section-dark");
+    cardTitleH2.classList.add("card-title-h2-dark");
+
     //TWIN_SECTION_DARK
 
-    const twinbg = document.querySelector(".section3-wrapper");
-    const twin3title = document.querySelector(".twin-section-title");
-    const twindivider = document.querySelector(".svg-divider");
-    const twinbg4 = document.querySelector(".section4");
-    const twin4title = document.querySelector(".twin-section2-title");
-    const twintabtitle = document.querySelector(".tab-title");
-    const twinclick = document.querySelector(".click-btn");
-    const twindatap = document.querySelectorAll(".data-paragraph");
+    //DI troppo perché c'è già su dichiarate//
+    // const twinbg = document.querySelector(".section3-wrapper");
+    // const twin3title = document.querySelector(".twin-section-title");
+    // const twindivider = document.querySelector(".svg-divider");
+    // const twinbg4 = document.querySelector(".section4");
+    // const twin4title = document.querySelector(".twin-section2-title");
+    // const twintabtitle = document.querySelector(".tab-title");
+    // const twinclick = document.querySelector(".click-btn");
+    // const twindatap = document.querySelectorAll(".data-paragraph");
 
     twinbg.classList.add("section3-wrapper-dark");
     twin3title.classList.add("twin-section-title-dark");
@@ -74,6 +81,10 @@ inputChangeMode.addEventListener("change", () => {
     herobg.classList.remove("hero-section-dark");
     herop.classList.remove("hero-homepage-text-parag-dark");
     heroWave.src = "./Components/HeroSection/images/wave-bg.svg";
+
+    //CARD_SECTION_LIGHT
+    cardSection.classList.remove("card-section-dark");
+    cardTitleH2.classList.remove("card-title-h2-dark");
 
     //TWIN_SECTION_LIGHT
     twinbg.classList.remove("section3-wrapper-dark");
