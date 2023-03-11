0a11fe2c3ea399b1623e228509efb2dd4674e6e1
marcobonni <marco.bonni19@gmail.com>
2023-03-11 15:12:16 +0100

    add dark mode and fix animation

--- Pancake/globalJS.js
+++ Pancake/globalJS.js
@@ -11,6 +11,9 @@ inputChangeMode.addEventListener("change", () => {
   const herobg = document.querySelector(".hero-section");
   const herop = document.querySelector(".hero-homepage-text-parag");
   const heroWave = document.getElementById("hero-wave");
+  //ORANGE_SECTION_DARK
+  const OrangeSection = document.querySelector(".orange-section")
+  const OrangeSectionDark = document.querySelector(".orange-section-dark")
 
   //DARK
   if (mode === true) {
@@ -30,6 +33,10 @@ inputChangeMode.addEventListener("change", () => {
     herop.classList.add("hero-homepage-text-parag-dark");
     heroWave.src = "./Components/HeroSection/images/wave-bg-d.svg";
 
+    //ORANGE_SECTION_DARK
+    OrangeSection.classList.add("orange-section-dark");
+    OrangeSection.classList.remove("orange-section");
+
     //LIGHT
   } else if (mode === false) {
     //GLOBAL LIGHT:
@@ -42,5 +49,10 @@ inputChangeMode.addEventListener("change", () => {
     herobg.classList.remove("hero-section-dark");
     herop.classList.remove("hero-homepage-text-parag-dark");
     heroWave.src = "./Components/HeroSection/images/wave-bg.svg";
+
+
+    //ORANGE_SECTION_LIGHT
+    OrangeSectionDark.classList.remove("orange-section-dark");
+    OrangeSectionDark.classList.add("orange-section");
   }
 });
