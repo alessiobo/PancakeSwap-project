12797429fc98ecfa2b853573195bbddd4f58761e
Cipriano Mereu <ciprianomereu@gmail.com>
2023-03-12 18:31:02 +0100

    add:hidden scrollbar

--- Pancake/Components/NavbarSettings/navbar-settings.js
+++ Pancake/Components/NavbarSettings/navbar-settings.js
@@ -2,10 +2,16 @@ const icon_settings = document.querySelector(".navbar-btn-option");
 const icon_X = document.querySelector(".nav-settings-icon-x");
 const settingsSection = document.querySelector(".nav-settings-section");
 
-icon_settings.addEventListener("click", () => {
-  settingsSection.classList.remove("hidden");
+const html = document.getElementById("html");
+document.addEventListener("DOMContentLoaded", function () {
+  icon_settings.addEventListener("click", () => {
+    settingsSection.classList.remove("hidden");
+
+    html.style.overflow = "hidden";
+  });
 });
 
 icon_X.addEventListener("click", () => {
   settingsSection.classList.add("hidden");
+  html.style.overflow = "auto";
 });
